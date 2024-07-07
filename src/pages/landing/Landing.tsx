9ae5dfffeca01334b461f508048174e04036e05f
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-07 22:03:13 +0330

    initial commit

---
+++ src/pages/landing/Landing.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/header/Header";
+import Footer from "@/components/footer/Footer";
+export function Landing() {
+   return (
+      <>
+         <p>Landing page</p>
+      </>
+   );
+}
+
+export default Landing;
