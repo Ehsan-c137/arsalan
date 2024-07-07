1f2f475ca3b289f34f9d5069b9e7baa9ad7b4566
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-07 22:00:20 +0330

    add desktop & mobile header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,14 @@
+import logo from "/public/arsalan-logo.svg";
+import Image from "next/image";
+import Link from "next/link";
+import DesktopNav from "../navbar/DesktopNav";
+import MobileNav from "../navbar/MobileNav";
+
+export default function Header() {
+   return (
+      <header className="w-full bg-transparent hover:bg-white flex justify-between items-center sticky p-6 transition-[background] ">
+         <DesktopNav />
+         <MobileNav />
+      </header>
+   );
+}
