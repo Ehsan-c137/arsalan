7fa1be0e529d12c22f68d71d8ea138db064b8ba7
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-07 22:02:53 +0330

    initial commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,10 @@
+import DesktopNav from "./DesktopNav";
+import MobileNav from "./MobileNav";
+export default function Navbar() {
+   return (
+      <nav>
+         <DesktopNav />
+         <MobileNav />
+      </nav>
+   );
+}
