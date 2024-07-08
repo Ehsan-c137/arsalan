25f5645f1722a88c69ad8fd3ec00eb9931b60ad1
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-08 21:22:50 +0330

    create Accordion component

---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState } from "react";
+import PlusIcon from "@/assets/icons/plusIcon";
+import MinusIcon from "@/assets/icons/minusIcon";
+
+interface IAccordion {
+   children: React.ReactNode;
+   isLast?: boolean;
+   heading: string;
+}
+export default function Accordion({ children, isLast, heading }: IAccordion) {
+   const [isOpen, setIsOpen] = useState(false);
+
+   return (
+      <>
+         <div className="flex flex-col w-56">
+            <button
+               className={`group border-t ${
+                  isLast ? "border-b" : ""
+               } border-[#c1c1c2] focus:outline-none`}
+               onClick={() => setIsOpen(!isOpen)}
+            >
+               <div className="flex items-center justify-between h-12 px-3 font-semibold">
+                  <span className="truncate">{heading}</span>
+                  <div className="font-light">
+                     {isOpen ? <MinusIcon /> : <PlusIcon />}
+                  </div>
+               </div>
+               <div
+                  className={`overflow-hidden duration-500`}
+                  onClick={(e) => e.stopPropagation()}
+                  style={{
+                     maxHeight: isOpen ? "10rem" : "0rem",
+                  }}
+               >
+                  {children}
+               </div>
+            </button>
+         </div>
+      </>
+   );
+}
