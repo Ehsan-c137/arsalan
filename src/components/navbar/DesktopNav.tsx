0788dc2d2dca5e35e60ce174674b5757af18ff71
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-08 21:34:48 +0330

    add home Link to logo

--- src/components/navbar/DesktopNav.tsx
+++ src/components/navbar/DesktopNav.tsx
@@ -6,7 +6,9 @@ import logo from "/public/arsalan-logo.svg";
 export default function DesktopNav() {
    return (
       <nav className="hidden lg:flex w-full justify-between items-center">
-         <Image src={logo} alt="logo" width={250} />
+         <Link href={"/"}>
+            <Image src={logo} alt="logo" width={250} />
+         </Link>
          <ul className="flex items-center gap-12">
             <Link
                href="/shop"
