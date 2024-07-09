b9b1aa4386634ad5f58ae6e51d20efb92a9f1a61
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-09 19:02:08 +0330

    test collectionItems

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,7 +1,15 @@
+import CollectionsItem from "./collectionsItem";
 export default function Collections() {
    return (
-      <div className="flex-grow h-[350px] w-full bg-red-500">
-         <p>Collections</p>
+      <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+         {/* {data.map((item) => (
+            <CollectionsItem key={item.title} />
+         ))} */}
+         <CollectionsItem />
+         <CollectionsItem />
+         <CollectionsItem />
+         <CollectionsItem />
+         <CollectionsItem />
       </div>
    );
 }
