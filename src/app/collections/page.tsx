fa2df1116c038890ef8729887d5e06b0b17c2a74
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-09 22:22:26 +0330

    add mock data

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -1,15 +1,36 @@
 import CollectionsItem from "./collectionsItem";
 export default function Collections() {
    return (
-      <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
-         {/* {data.map((item) => (
-            <CollectionsItem key={item.title} />
-         ))} */}
-         <CollectionsItem />
-         <CollectionsItem />
-         <CollectionsItem />
-         <CollectionsItem />
-         <CollectionsItem />
+      <div className="flex flex-col gap-8">
+         <p className="text-2xl">ELECTRIC KETTLES</p>
+         <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            <CollectionsItem />
+            <CollectionsItem />
+         </div>
+         <p className="text-2xl">TEA MAKER</p>
+         <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            <CollectionsItem />
+            <CollectionsItem />
+         </div>
+
+         <p className="text-2xl">COFFEE MAKERS</p>
+         <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            <CollectionsItem />
+            <CollectionsItem />
+         </div>
+         <p className="text-2xl">SAMOVARS</p>
+         <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            <CollectionsItem />
+            <CollectionsItem />
+            <CollectionsItem />
+            <CollectionsItem />
+         </div>
+         <p className="text-2xl">BREAD MAKERS</p>
+         <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            <CollectionsItem />
+            <CollectionsItem />
+            <CollectionsItem />
+         </div>
       </div>
    );
 }
