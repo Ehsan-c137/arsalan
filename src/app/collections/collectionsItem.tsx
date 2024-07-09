6e72f619a3ed3c327d51bd3efb92e21a7e6650db
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-09 13:00:19 +0330

    create collectionItem Card

---
+++ src/app/collections/collectionsItem.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
+
+interface IItem {
+   title: string;
+}
+
+export default function CollectionsItem() {
+   return (
+      <Link href={"/"} className="space-y-2 grid">
+         <Image
+            src={"/"}
+            alt=""
+            width={250}
+            height={250}
+            className="bg-slate-300"
+         />
+         <div className="flex flex-col">
+            <h4 className="text-xl">Bread maker</h4>
+            <p className="text-base">95$</p>
+         </div>
+      </Link>
+   );
+}
