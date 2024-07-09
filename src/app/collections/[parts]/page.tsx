6f5a458bdcac164861b260da5c2f04e5336b4366
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-09 19:06:46 +0330

    fix page props type

--- src/app/collections/[parts]/page.tsx
+++ src/app/collections/[parts]/page.tsx
@@ -1,3 +1,9 @@
-export default function parts(params) {
+type pageProps = {
+   params: {
+      parts: string;
+   };
+};
+
+export default function parts(params: pageProps) {
    return <p>{params.params.parts}</p>;
 }
