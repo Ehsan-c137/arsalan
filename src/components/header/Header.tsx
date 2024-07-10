5a6266f13c90a51fa1787c607b5efe9f7026cf5c
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-10 20:57:29 +0330

    add dark logo

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,19 @@
+"use client";
 import logo from "/public/arsalan-logo.svg";
 import Image from "next/image";
 import Link from "next/link";
 import DesktopNav from "../navbar/DesktopNav";
 import MobileNav from "../navbar/MobileNav";
-
+import { usePathname } from "next/navigation";
 export default function Header() {
+   const pathname = usePathname();
+
    return (
-      <header className="w-full flex justify-between items-center text-white fixed top-0 p-6 transition-[background] z-50 ">
+      <header
+         className={`w-full flex justify-between items-center ${
+            pathname === "/" ? "text-white" : "text-black"
+         } fixed top-0 p-6 transition-[background] z-50`}
+      >
          <DesktopNav />
          <MobileNav />
       </header>
