20a7968372c0279a8c0793a37dba252a83db0bac
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-10 20:17:42 +0330

    comment the collection item for test

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -4,32 +4,32 @@ export default function Collections() {
       <div className="flex flex-col gap-8">
          <p className="text-2xl">ELECTRIC KETTLES</p>
          <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
-            <CollectionsItem />
-            <CollectionsItem />
+            {/* <CollectionsItem />
+            <CollectionsItem /> */}
          </div>
          <p className="text-2xl">TEA MAKER</p>
          <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
-            <CollectionsItem />
-            <CollectionsItem />
+            {/* <CollectionsItem />
+            <CollectionsItem /> */}
          </div>
 
          <p className="text-2xl">COFFEE MAKERS</p>
          <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
-            <CollectionsItem />
-            <CollectionsItem />
+            {/* <CollectionsItem />
+            <CollectionsItem /> */}
          </div>
          <p className="text-2xl">SAMOVARS</p>
          <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            {/* <CollectionsItem />
             <CollectionsItem />
             <CollectionsItem />
-            <CollectionsItem />
-            <CollectionsItem />
+            <CollectionsItem /> */}
          </div>
          <p className="text-2xl">BREAD MAKERS</p>
          <div className="grid lg:grid-cols-3 gap-x-14 gap-y-10">
+            {/* <CollectionsItem />
             <CollectionsItem />
-            <CollectionsItem />
-            <CollectionsItem />
+            <CollectionsItem /> */}
          </div>
       </div>
    );
