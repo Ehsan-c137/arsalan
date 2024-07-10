381fdf781ff62d348a9daf609805b8fd5f773957
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-10 20:57:38 +0330

    add dark logo

--- src/components/navbar/DesktopNav.tsx
+++ src/components/navbar/DesktopNav.tsx
@@ -1,15 +1,20 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import ShoppingCart from "@/assets/icons/shoppingCart";
 import logoDark from "/public/arsalan-logo-dark.svg";
 import logo from "/public/arsalan-logo.png";
+import { usePathname } from "next/navigation";
 
 export default function DesktopNav() {
+   const pathname = usePathname();
+   const headerLogo = pathname === "/" ? logo : logoDark;
    return (
       <nav className="hidden lg:flex w-full justify-between items-center">
          <Link href={"/"}>
-            <Image src={logo} alt="logo" width={250} />
+            <Image src={headerLogo} alt="logo" width={250} />
          </Link>
+
          <ul className="flex items-center gap-12">
             <Link
                href="/collections"
