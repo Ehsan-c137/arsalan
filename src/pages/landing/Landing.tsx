20f3e4d564e85a4eb1cb1074298d178d0cfc7d4c
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-10 17:35:53 +0330

    add products image & videos

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,13 +1,20 @@
+"use client";
+
+import { Video } from "@/components/video/Video";
+import Image from "next/image";
+
 export function Landing() {
    return (
       <div className="flex flex-col w-full">
-         <div className="hero h-[100vh] w-full flex items-end">
-            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6">
+         <div className="hero h-[100vh] w-full flex items-end relative object-cover">
+            <Video src="/videos/home-section-tea-maker-big.mp4" />
+            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
                <div className="space-y-2">
-                  <p className="text-3xl">ARSALAN</p>
-                  <p className="text-6xl">Lorem ipsum dolor sit amet. </p>
+                  <p className="text-3xl">SAKI</p>
+                  <p className="text-6xl">Turkish Tea Maker.</p>
                   <p className="text-2xl">
-                     Elevating your tea experience to new heights.
+                     Freshness redefined. Elevating your tea experience to new
+                     heights.
                   </p>
                </div>
                <button className="border-2 pt-2 pb-2 pl-4 pr-4 border-red-500 bg-transparent text-white hover:bg-red-500 transition-[background] duration-400">
@@ -15,10 +22,11 @@ export function Landing() {
                </button>
             </div>
          </div>
+         {/*  */}
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
             <div className="bg-red-600 pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
                <div>
-                  <p className="font-size-14px text-white">NEW</p>
+                  <p className="font-size-14px text-white">New</p>
                   <p className="text-6xl text-white">Turkish Tea Maker</p>
                </div>
                <p className="text-2xl text-white">
@@ -36,24 +44,154 @@ export function Landing() {
                   </button>
                </div>
             </div>
-            <div className="bg-black h-full w-full col-span-1">asdf</div>
+            <div className="bg-black h-full w-full col-span-1">
+               <Image
+                  width={0}
+                  height={0}
+                  style={{
+                     width: "100%",
+                  }}
+                  sizes="100vw"
+                  src="/images/home-teamaker-section-desktop.webp"
+                  alt="home tea maker"
+               />
+            </div>
+         </div>
+         {/*  */}
+         <div className="hero h-[100vh] w-full flex items-end relative object-cover">
+            <Video src="/videos/home-section-baristan-big.mp4" />
+            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
+               <div className="space-y-2">
+                  <p className="text-6xl">Baristan</p>
+                  <p className="text-3xl">Precision in Every Pour</p>
+               </div>
+               <button className="border-2 pt-2 pb-2 pl-4 pr-4 border-red-500 bg-transparent text-white hover:bg-red-500 transition-[background] duration-400">
+                  EXPLORE
+               </button>
+            </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className="pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center items-start ">
-               <p className="text-6xl text-black">ARSALAN Products</p>
+            <div className="pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center items-start bg-[#161a1d] text-white">
+               <p className="text-6xl ">
+                  Baristan <br /> Electric Gooseneck Kettle
+               </p>
 
-               <p className="text-2xl text-black">
+               <p className="text-2xl ">
+                  Master the perfect pour. <br />
+                  Unrestricted flow for unrivaled coffee precision.
+               </p>
+
+               <button className="border-2 border-red-600 pt-2 pb-2 pr-4 pl-4 hover:text-white hover:bg-red-600 transition-colors">
+                  DISCOVER
+               </button>
+            </div>
+            <div className="bg-black h-full w-full col-span-1">
+               <Image
+                  width={0}
+                  height={0}
+                  style={{
+                     width: "100%",
+                  }}
+                  sizes="100vw"
+                  src="/images/home-baristan-section-desktop.webp"
+                  alt="home baristan"
+               />
+            </div>
+         </div>
+         <div className="hero h-[100vh] w-full flex items-end relative object-cover">
+            <Video src="/videos/home-section-luna-big.mp4" />
+            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
+               <div className="space-y-2">
+                  <p className="text-6xl">Luna</p>
+                  <p className="text-3xl">7 Pressets, Infinite Possibilities</p>
+               </div>
+               <button className="border-2 pt-2 pb-2 pl-4 pr-4 border-red-500 bg-transparent text-white hover:bg-red-500 transition-[background] duration-400">
+                  EXPLORE
+               </button>
+            </div>
+         </div>
+         <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
+            <div className="bg-white text-black pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+               <div>
+                  <h2 className="text-6xl font-bold">
+                     Luna <br />
+                     Adjustable Temperature Kettle
+                  </h2>
+               </div>
+               <p className="text-2xl ">
                   Elevate your tea experience with Turkish Tea Maker, fusing
                   <br />
                   innovation and tradition for authentic flavors sand a sleek
                   design.
                </p>
-
-               <button className="border-2 border-red-600 pt-2 pb-2 pr-4 pl-4 text-black hover:text-white hover:bg-red-600 transition-colors">
-                  DISCOVER
+               <div>
+                  <button className="border-2 border-red-500  pt-2 pb-2 pr-4 pl-4  hover:text-red-600 hover:bg-white transition-colors">
+                     DISCOVER
+                  </button>
+                  <button className="pt-2 pb-2 pr-4 pl-4 text-white">
+                     SHOP NOW
+                  </button>
+               </div>
+            </div>
+            <div className="bg-black h-full w-full col-span-1">
+               <Image
+                  width={0}
+                  height={0}
+                  style={{
+                     width: "100%",
+                  }}
+                  sizes="100vw"
+                  src="/images/home-luna-section-desktop.webp"
+                  alt="home tea maker"
+               />
+            </div>
+         </div>
+         <div className="hero h-[100vh] w-full flex items-end relative object-cover">
+            <Video src="/videos/home-section-samovar-big.mp4" />
+            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
+               <div className="space-y-2">
+                  <p className="text-6xl">Electric Russian Samovar</p>
+                  <p className="text-3xl">
+                     A modern touch to timeless traditions.
+                  </p>
+               </div>
+               <button className="border-2 pt-2 pb-2 pl-4 pr-4 border-red-500 bg-transparent text-white hover:bg-red-500 transition-[background] duration-400">
+                  EXPLORE
                </button>
             </div>
-            <div className="bg-black h-full w-full col-span-1">asdf</div>
+         </div>
+         <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
+            <div className=" text-white bg-[#56101a] pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+               <div>
+                  <h2 className="text-6xl font-bold">
+                     Electric Russian Samovar
+                  </h2>
+               </div>
+               <p className="text-2xl ">
+                  Introducing the future of tea making. Traditional taste,
+                  advanced brew.
+               </p>
+               <div>
+                  <button className="border-2 border-red-500  pt-2 pb-2 pr-4 pl-4  hover:text-red-600 hover:bg-white transition-colors">
+                     DISCOVER
+                  </button>
+                  <button className="pt-2 pb-2 pr-4 pl-4 text-white">
+                     SHOP NOW
+                  </button>
+               </div>
+            </div>
+            <div className="bg-black h-full w-full col-span-1">
+               <Image
+                  width={0}
+                  height={0}
+                  style={{
+                     width: "100%",
+                  }}
+                  sizes="100vw"
+                  src="/images/home-electric-samovar-section-desktop.webp"
+                  alt="home tea maker"
+               />
+            </div>
          </div>
       </div>
    );
