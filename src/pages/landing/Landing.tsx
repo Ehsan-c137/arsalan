a67ca66ef344cc38b02678875a68227f49fa5220
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-10 20:10:56 +0330

    add product

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import { Video } from "@/components/video/Video";
 import Image from "next/image";
+import Link from "next/link";
 
 export function Landing() {
    return (
@@ -24,7 +23,7 @@ export function Landing() {
          </div>
          {/*  */}
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className="bg-red-600 pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+            <div className="bg-red-600 pl-6 py-10 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
                <div>
                   <p className="font-size-14px text-white">New</p>
                   <p className="text-6xl text-white">Turkish Tea Maker</p>
@@ -44,12 +43,14 @@ export function Landing() {
                   </button>
                </div>
             </div>
-            <div className="bg-black h-full w-full col-span-1">
+            <div className="bg-black h-full w-full col-span-1 ">
                <Image
                   width={0}
                   height={0}
                   style={{
                      width: "100%",
+                     height: "100%",
+                     objectFit: "cover",
                   }}
                   sizes="100vw"
                   src="/images/home-teamaker-section-desktop.webp"
@@ -62,7 +63,7 @@ export function Landing() {
             <Video src="/videos/home-section-baristan-big.mp4" />
             <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
                <div className="space-y-2">
-                  <p className="text-6xl">Baristan</p>
+                  <p className="text-5xl md:text-6xl">Baristan</p>
                   <p className="text-3xl">Precision in Every Pour</p>
                </div>
                <button className="border-2 pt-2 pb-2 pl-4 pr-4 border-red-500 bg-transparent text-white hover:bg-red-500 transition-[background] duration-400">
@@ -71,12 +72,12 @@ export function Landing() {
             </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className="pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center items-start bg-[#161a1d] text-white">
-               <p className="text-6xl ">
+            <div className="pl-6 py-10 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center items-start bg-[#161a1d] text-white">
+               <p className="text-4xl md:text-6xl">
                   Baristan <br /> Electric Gooseneck Kettle
                </p>
 
-               <p className="text-2xl ">
+               <p className="text-lg md:text-2xl">
                   Master the perfect pour. <br />
                   Unrestricted flow for unrivaled coffee precision.
                </p>
@@ -91,6 +92,8 @@ export function Landing() {
                   height={0}
                   style={{
                      width: "100%",
+                     height: "100%",
+                     objectFit: "cover",
                   }}
                   sizes="100vw"
                   src="/images/home-baristan-section-desktop.webp"
@@ -111,14 +114,14 @@ export function Landing() {
             </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className="bg-white text-black pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+            <div className="bg-white text-black pl-6 py-10 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
                <div>
-                  <h2 className="text-6xl font-bold">
+                  <h2 className="text-4xl md:text-6xl font-bold">
                      Luna <br />
                      Adjustable Temperature Kettle
                   </h2>
                </div>
-               <p className="text-2xl ">
+               <p className="text-lg md:text-2xl ">
                   Elevate your tea experience with Turkish Tea Maker, fusing
                   <br />
                   innovation and tradition for authentic flavors sand a sleek
@@ -139,6 +142,8 @@ export function Landing() {
                   height={0}
                   style={{
                      width: "100%",
+                     height: "100%",
+                     objectFit: "cover",
                   }}
                   sizes="100vw"
                   src="/images/home-luna-section-desktop.webp"
@@ -150,8 +155,10 @@ export function Landing() {
             <Video src="/videos/home-section-samovar-big.mp4" />
             <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
                <div className="space-y-2">
-                  <p className="text-6xl">Electric Russian Samovar</p>
-                  <p className="text-3xl">
+                  <p className="text-4xl md:text-6xl">
+                     Electric Russian Samovar
+                  </p>
+                  <p className="text-lg md:text-3xl">
                      A modern touch to timeless traditions.
                   </p>
                </div>
@@ -161,13 +168,13 @@ export function Landing() {
             </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className=" text-white bg-[#56101a] pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+            <div className="text-white bg-[#56101a] pl-6 lg:pl-10 py-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
                <div>
-                  <h2 className="text-6xl font-bold">
+                  <h2 className="text-4xl md:text-6xl font-bold">
                      Electric Russian Samovar
                   </h2>
                </div>
-               <p className="text-2xl ">
+               <p className="text-lg md:text-2xl ">
                   Introducing the future of tea making. Traditional taste,
                   advanced brew.
                </p>
@@ -186,11 +193,85 @@ export function Landing() {
                   height={0}
                   style={{
                      width: "100%",
+                     height: "100%",
+                     objectFit: "cover",
                   }}
                   sizes="100vw"
                   src="/images/home-electric-samovar-section-desktop.webp"
                   alt="home tea maker"
                />
+            </div>
+         </div>
+         <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
+            <div className=" text-black pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+               <div>
+                  <h2 className="text-4xl md:text-6xl font-bold">
+                     SAKI Products
+                  </h2>
+               </div>
+               <p className="text-lg md:text-2xl">
+                  Delivering the ultimate coffee and tea experience through
+                  innovation and thoughtful design.
+               </p>
+               <div>
+                  <button className="border-2 border-red-500  pt-2 pb-2 pr-4 pl-4  hover:text-red-600 hover:bg-white transition-colors">
+                     DISCOVER
+                  </button>
+               </div>
+            </div>
+            <div className="text-black h-full w-full col-span-1 flex items-center gap-4 overflow-x-hidden justify-center">
+               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                  <Link href="/products/pour-over-carafe">
+                     <div className="bg-[#f5f5f7]  h-[300px] flex items-center justify-center">
+                        <Image
+                           src={"/images/pour-over-carafe.webp"}
+                           alt="pour over carafe"
+                           width={200}
+                           height={200}
+                        />
+                     </div>
+                  </Link>
+                  <Link href={"/products/luna-precision-electric-kettle"}>
+                     <div className="bg-[#f5f5f7] w-[300px] h-[300px] flex items-center justify-center">
+                        <Image
+                           src={"/images/baristan-electric-kettle.webp"}
+                           alt="baristan-electric-kettle"
+                           width={200}
+                           height={200}
+                        />
+                     </div>
+                  </Link>
+                  <Link href={"/products/electric-samovar"}>
+                     <div className="bg-[#f5f5f7] w-[300px] h-[300px] flex items-center justify-center">
+                        <Image
+                           src={"/images/electric-samovar.webp"}
+                           alt="electric samovar"
+                           width={200}
+                           height={200}
+                        />
+                     </div>
+                  </Link>
+                  <Link href={"/products/baristan-electric-kettle"}>
+                     <div className="bg-[#f5f5f7] w-[300px] h-[300px] flex items-center justify-center">
+                        <Image
+                           src={"/images/baristan-electric-kettle-2.webp"}
+                           alt="baristan-electric-kettle"
+                           width={200}
+                           height={200}
+                        />
+                     </div>
+                  </Link>
+                  <Link href={"/products/electric-samovar"}>
+                     <div className="bg-[#f5f5f7] w-[300px] h-[300px] flex items-center justify-center">
+                        <Image
+                           src={"/images/electric-samovar.webp"}
+                           alt="baristan-electric-kettle"
+                           width={200}
+                           height={200}
+                        />
+                     </div>
+                  </Link>
+               </div>
             </div>
          </div>
       </div>
