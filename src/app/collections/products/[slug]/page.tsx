186e2983adf8fc6cc04631c4507b012cb12c50cb
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-11 16:26:55 +0330

    make data dynamic

--- src/app/collections/products/[slug]/page.tsx
+++ src/app/collections/products/[slug]/page.tsx
@@ -1,3 +1,6 @@
+import { allProducts } from "@/constant";
+import CollectionsItem from "../../collectionsItem";
+
 type pageProps = {
    params: {
       slug: string;
@@ -5,5 +8,20 @@ type pageProps = {
 };
 
 export default function Page(slug: pageProps) {
-   return <div>Page: {slug.params.slug}</div>;
+   const products = allProducts.find(
+      (product) => product.url === slug.params.slug
+   );
+
+   return (
+      <div className="flex flex-wrap gap-8">
+         {products?.items.map((item) => (
+            <CollectionsItem
+               key={item.title}
+               price={item.price}
+               title={item.title}
+               img={`${item.imgSrc}`}
+            />
+         ))}
+      </div>
+   );
 }
