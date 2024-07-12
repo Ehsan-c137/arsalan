2275b8883e0becf1d877958f83531c30b1914b4a
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-12 22:46:02 +0330

    fix style in small screen

--- src/app/collections/page.tsx
+++ src/app/collections/page.tsx
@@ -8,7 +8,7 @@ export default function Collections() {
             return (
                <div className="flex flex-col gap-8" key={products.category}>
                   <p className="text-2xl tracking-wider">{products.category}</p>
-                  <div className="grid lg:grid-cols-3 gap-x-14 gap-y-16">
+                  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-14 gap-y-16">
                      {products.items.map((item) => {
                         const url = item.title.replace(/ /g, "-").toLowerCase();
                         console.log(item.imgSrc[0]);
