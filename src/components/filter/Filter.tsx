9c5e41504e42f75beb7d70b7e4f2a0f309d34c9f
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-13 22:47:57 +0330

    fix highlighting -all- when on the products,parts section

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -8,10 +8,14 @@ const shopItems = [
    {
       heading: "products",
       children: [
+         {
+            name: "all",
+            href: "/",
+         },
          ...allProducts.map((item) => {
             return {
                name: item.category?.toLocaleLowerCase(),
-               href: item.url,
+               href: `/${item.url}`,
             };
          }),
       ],
@@ -19,10 +23,14 @@ const shopItems = [
    {
       heading: "PARTs",
       children: [
+         {
+            name: "all",
+            href: "/",
+         },
          ...allParts.map((item) => {
             return {
                name: item.category?.toLocaleLowerCase(),
-               href: `/parts/${item.url}`,
+               href: `/${item.url}`,
             };
          }),
       ],
@@ -36,40 +44,71 @@ export default function Filter() {
    const { isOpen, setIsOpen } = useDrawer();
    return (
       <div className="pr-8 pb-8">
-         {shopItems.map((item) => (
-            <Accordion
-               key={item.heading}
-               heading={item.heading}
-               isLast={item.isLast}
-            >
-               {item.children.map((child) => {
-                  const isAcitve =
-                     `${`/collections/products/${child.href}`}` === pathname;
+         {shopItems.map((item) => {
+            const isPartsSection = item.heading === "PARTs";
 
-                  return (
-                     <p
-                        className={`flex items-center h-8 px-4 text-sm hover:text-red-600 transition-opacity truncate ${
-                           isAcitve && "text-red-600"
-                        }`}
-                        onClick={() => {
-                           isOpen && setIsOpen(false);
-                           const isPartsSection = item.heading === "PARTs";
-                           if (isPartsSection) {
-                              router.push(`/collections/${child.href}`);
-                           } else {
-                              router.push(
-                                 `/collections/products/${child.href}`
-                              );
-                           }
-                        }}
-                        key={child.href}
-                     >
-                        {child.name}
-                     </p>
-                  );
-               })}
-            </Accordion>
-         ))}
+            return (
+               <Accordion
+                  key={item.heading}
+                  heading={item.heading}
+                  isLast={item.isLast}
+               >
+                  {item.children.map((child) => {
+                     const itemUrl = `/collections/${item.heading.toLowerCase()}${
+                        child.href.endsWith("/") ? "" : "/"
+                     }`;
+                     console.log(itemUrl, pathname);
+                     const isAcitve = itemUrl === pathname;
+
+                     // const isAllProductSection =
+                     //    "/collections/products" === pathname &&
+                     //    child.name.toLowerCase() === "all";
+
+                     // const isAllPartsSecton =
+                     //    "/collections/parts" === pathname &&
+                     //    child.name.toLowerCase() === "all";
+
+                     // console.log(pathname);
+
+                     return (
+                        <p
+                           className={`flex items-center h-8 px-4 text-gray-500 text-sm hover:opacity-70 transition-opacity truncate capitalize ${
+                              isAcitve ? "!font-semibold" : "font-normal"
+                           }`}
+                           onClick={() => {
+                              isOpen && setIsOpen(false);
+                              // router.push(
+                              //    `/collections/${item.heading}/${child.href}`
+                              // );
+                              const isPartsSection =
+                                 item.heading.toLowerCase() === "parts";
+                              if (isPartsSection) {
+                                 if (child.name.toLowerCase() === "all") {
+                                    router.push("/collections/parts");
+                                 } else {
+                                    router.push(
+                                       `/collections/parts/${child.href}`
+                                    );
+                                 }
+                              } else {
+                                 if (child.name.toLowerCase() === "all") {
+                                    router.push("/collections/products");
+                                 } else {
+                                    router.push(
+                                       `/collections/products/${child.href}`
+                                    );
+                                 }
+                              }
+                           }}
+                           key={child.href}
+                        >
+                           {child.name}
+                        </p>
+                     );
+                  })}
+               </Accordion>
+            );
+         })}
       </div>
    );
 }
