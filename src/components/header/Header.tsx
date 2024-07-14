d1d94b3b5afd4791d810b234ab2fd6481e0cf645
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-14 18:58:11 +0330

    change logo on hover the header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,21 +1,30 @@
 "use client";
-import logo from "/public/arsalan-logo.svg";
-import Image from "next/image";
-import Link from "next/link";
+
 import DesktopNav from "../navbar/DesktopNav";
 import MobileNav from "../navbar/MobileNav";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
+import CollectionsItem from "@/app/collections/collectionsItem";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Header() {
    const pathname = usePathname();
+   // change logo on hover
+   const [isHovered, setIsHovered] = useState<boolean>(false);
 
    return (
-      <header
-         className={`w-full flex justify-between items-center ${
-            pathname === "/" ? "text-white" : "text-black"
-         } fixed top-0 p-6 transition-[background] z-50`}
-      >
-         <DesktopNav />
-         <MobileNav />
-      </header>
+      <div className="flex flex-col">
+         <header
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            className={`w-full flex justify-between hover:bg-white items-center pointer-events-none ${
+               pathname === "/" ? "text-white" : "text-black"
+            } fixed top-0 px-6 z-50`}
+         >
+            <DesktopNav isHovered={isHovered} />
+            <MobileNav />
+         </header>
+      </div>
    );
 }
