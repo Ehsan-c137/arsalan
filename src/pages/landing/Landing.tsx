0ae8101106b0e0b774945c5e6d753a1fad48b518
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-07-14 15:31:24 +0330

    create blog section

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import ProductsSwiper from "./ProductsSwiper";
 import ChevronRightIcon from "@/assets/icons/chevron-right";
+import BlogSwiper from "./BlogSwiper";
 
 export function Landing() {
    return (
@@ -142,9 +143,9 @@ export function Landing() {
             </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className="bg-white text-black pl-6 py-10 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
+            <div className="bg-white text-black lg:pl-10 lg:pr-6 px-6 py-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
                <div>
-                  <h2 className="text-4xl md:text-6xl font-bold">
+                  <h2 className="text-4xl md:text-6xl font-semibold">
                      Luna <br />
                      Adjustable Temperature Kettle
                   </h2>
@@ -155,12 +156,15 @@ export function Landing() {
                   innovation and tradition for authentic flavors sand a sleek
                   design.
                </p>
-               <div>
+               <div className="flex items-center gap-8">
                   <button className="border-2 border-red-600 px-10 py-2 btn-effect">
                      DISCOVER
                   </button>
-                  <button className="pt-2 pb-2 pr-4 pl-4 text-white">
-                     SHOP NOW
+                  <button className="flex items-center gap-2 hover:text-red-600">
+                     SHOP
+                     <span className="text-red-600">
+                        <ChevronRightIcon />
+                     </span>
                   </button>
                </div>
             </div>
@@ -181,7 +185,7 @@ export function Landing() {
          </div>
          <div className="hero h-[100vh] w-full flex items-end relative object-cover">
             <Video src="/videos/home-section-samovar-big.mp4" />
-            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 space-y-6 absolute z-50 text-white">
+            <div className="pb-[30px] lg:pb-[100px] pl-6 lg:pl-10 flex flex-col gap-10 absolute z-50 text-white">
                <div className="space-y-2">
                   <p className="text-4xl md:text-6xl">
                      Electric Russian Samovar
@@ -191,29 +195,34 @@ export function Landing() {
                   </p>
                </div>
                <Link href="/collections">
-                  <button className="border-2 pt-2 pb-2 pl-4 pr-4 border-red-500 bg-transparent text-white hover:bg-red-500 transition-[background] duration-400">
+                  <button className="border-2 px-10 py-2 btn-effect border-red-600">
                      EXPLORE
                   </button>
                </Link>
             </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className="text-white bg-[#56101a] pl-6 lg:pl-10 py-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
-               <div>
-                  <h2 className="text-4xl md:text-6xl font-bold">
-                     Electric Russian Samovar
-                  </h2>
+            <div className="text-white bg-[#56101a] pl-6 lg:pl-10 py-10 h-full w-full col-span-1 flex flex-col gap-10 justify-center">
+               <div className="flex flex-col gap-4">
+                  <div>
+                     <h2 className="text-4xl md:text-6xl font-bold ">
+                        Electric Russian Samovar
+                     </h2>
+                  </div>
+                  <p className="text-lg md:text-2xl ">
+                     Introducing the future of tea making. Traditional taste,
+                     advanced brew.
+                  </p>
                </div>
-               <p className="text-lg md:text-2xl ">
-                  Introducing the future of tea making. Traditional taste,
-                  advanced brew.
-               </p>
-               <div>
-                  <button className="border-2 border-red-500  pt-2 pb-2 pr-4 pl-4  hover:text-red-600 hover:bg-white transition-colors">
+               <div className="flex gap-6">
+                  <button className="border-2 border-red-600 btn-effect py-2 px-10">
                      DISCOVER
                   </button>
-                  <button className="pt-2 pb-2 pr-4 pl-4 text-white">
-                     SHOP NOW
+                  <button className="flex items-center hover:text-red-600 duration-300 gap-2 pt-2 pb-2 pr-4 pl-4 text-">
+                     SHOP
+                     <span className="text-red-600">
+                        <ChevronRightIcon />
+                     </span>
                   </button>
                </div>
             </div>
@@ -233,20 +242,22 @@ export function Landing() {
             </div>
          </div>
          <div className="grid grid-cols-1 lg:grid-cols-2 h-[100vh]">
-            <div className=" text-black pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-4 justify-center">
-               <div>
-                  <h2 className="text-4xl md:text-6xl font-bold">
-                     SAKI Products
-                  </h2>
+            <div className=" text-black pl-6 lg:pl-10 h-full w-full col-span-1 flex flex-col gap-10 justify-center">
+               <div className="flex flex-col gap-4">
+                  <div>
+                     <h2 className="text-4xl md:text-6xl font-bold">
+                        SAKI Products
+                     </h2>
+                  </div>
+                  <p className="text-lg md:text-2xl">
+                     Delivering the ultimate coffee and tea experience through
+                     innovation and thoughtful design.
+                  </p>
                </div>
-               <p className="text-lg md:text-2xl">
-                  Delivering the ultimate coffee and tea experience through
-                  innovation and thoughtful design.
-               </p>
                <div>
                   <Link href="/collections">
-                     <button className="border-2 border-red-500  pt-2 pb-2 pr-4 pl-4  hover:text-red-600 hover:bg-white transition-colors">
-                        DISCOVER
+                     <button className="border-2 border-red-600  py-2 px-10 tracking-wider hover:text-white btn-effect">
+                        EXPLORE
                      </button>
                   </Link>
                </div>
@@ -317,6 +328,92 @@ export function Landing() {
                            width={200}
                            height={200}
                         />
+                     </div>
+                  </Link>
+                  <Link
+                     href={"/collections/products"}
+                     className="min-w-[150px] min-h-[150px] max-w-[300px] max-h-[300px]"
+                  >
+                     <div className="bg-[#f5f5f7] w-[300px] h-[300px] flex items-center justify-center">
+                        <Image
+                           src={"/images/dropdown-shop-all.svg"}
+                           alt="shop"
+                           width={200}
+                           height={200}
+                        />
+                     </div>
+                  </Link>
+               </div>
+            </div>
+         </div>
+         <div className="flex flex-col gap-16 items-center justify-center overflow-x-hidden h-[100vh] bg-[#f5f5f7]">
+            <div className="flex flex-col gap-10 text-center">
+               <div className="flex flex-col gap-6">
+                  <h2 className="text-5xl font-bold">Blog Highlights</h2>
+                  <p className="text-[#858585] text-2xl">
+                     Our digital blog hub curates guides, recipes, and insights
+                     to enhance your product experience.
+                  </p>
+               </div>
+               <div className="overflow-x-hidden flex gap-10">
+                  <Link href="/blog">
+                     <div className="flex flex-col gap-4 bg-white rounded-xl p-4 w-[350px]">
+                        <Image
+                           height={260}
+                           width={350}
+                           style={{
+                              borderRadius: "12px",
+                           }}
+                           alt="blog"
+                           src="/images/blog/1.webp"
+                        />
+
+                        <div className="flex flex-col gap-1 text-left">
+                           <p className="hover:underline">
+                              The World of Tea: Global Tea Rituals and Tea Times
+                           </p>
+                           <p className="text-[#858585]">By SAKI Team</p>
+                        </div>
+                     </div>
+                  </Link>
+                  <Link href="/blog">
+                     <div className="flex flex-col gap-4 bg-white rounded-xl p-4 w-[350px]">
+                        <Image
+                           height={260}
+                           width={350}
+                           style={{
+                              borderRadius: "12px",
+                           }}
+                           alt="blog"
+                           src="/images/blog/1.webp"
+                        />
+
+                        <div className="flex flex-col gap-1 text-left">
+                           <p className="hover:underline">
+                              The World of Tea: Global Tea Rituals and Tea Times
+                           </p>
+                           <p className="text-[#858585]">By SAKI Team</p>
+                        </div>
+                     </div>
+                  </Link>
+                  <Link href="/blog">
+                     <div className="flex flex-col gap-4 bg-white rounded-xl p-4 w-[350px]">
+                        <Image
+                           height={260}
+                           width={350}
+                           style={{
+                              borderRadius: "12px",
+                           }}
+                           alt="blog"
+                           src="/images/blog/1.webp"
+                        />
+
+                        <div className="flex flex-col gap-1 text-left">
+                           <p className="hover:underline">
+                              The World of Tea: Global Tea Rituals and Tea Times
+                           </p>
+                           <p className="text-[#858585]">By SAKI Team</p>
+                        </div>
                      </div>
                   </Link>
                </div>
